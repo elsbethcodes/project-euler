5d4d5e381705b5f177411e81e2efbc00c2274ad7
Elsbeth Horgan <117280903+elsbethcodes@users.noreply.github.com>
2025-06-29 09:33:10 +0100

    Update solution.py

--- problem_1/solution.py
+++ problem_1/solution.py
@@ -11,26 +11,31 @@
 
 ## Formulae:
 # Define a : first term, d : common difference and n : number of terms.
-# Then the last term of an arithmetic sequence, l = a + (n-1)d.
-# The summation of an arithmetic sequence, Sn = (a+l)/2 * n. 
+# Then the last term of an arithmetic sequence, L = a + (n-1)d.
+# The summation of an arithmetic sequence up to term n, Sn = (a+L)/2 * n. 
 # Or in words, Sn is the average of the first term and last term multiplied by the number of terms.
 
 ## Method:
 def first_term(d):
-  return d # In this case a=d
+  # Returns the first term of the sequence, a
+  return d # Note that in this problem a=d
 
 def number_of_terms(d,X):
-  # if X is a multiple of d then decrease the number of terms by 1 to exclude X
+  # Returns the number of terms, n 
+  # Let X be the number below which we will sum all terms
+  # If X is a multiple of d then decrease the number of terms by 1 to exclude X
   if X%d == 0: 
     return int(X/d) - 1
-  # otherwise round down to the nearest integer when d divides X
+  # Otherwise round down to the nearest integer when d divides X
   else:
     return int(X/d) 
 
 def last_term(d,X):
+  # Returns the last term of the sequence, L
   return first_term(d) + (number_of_terms(d,X) - 1) * d
 
 def summation(d,X):
+  # Returns the output of the summation formula
   return ((first_term(d) + last_term(d,X))/2 * number_of_terms(d,X))
 
 ## Test Case:
